d84db0ea376e24cf6ed6cc6e535981b5e7011aae
soda <soda@192.168.68.96>
2025-03-23 23:11:12 +0800

    first commit

---
+++ app/Models/Url.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Url extends Model
+{
+    protected $fillable = [
+        "title",
+        "url",
+        "cover",
+        "status",
+        "icon",
+        "is_recommended"
+    ];
+
+    public function url_groups(){
+        return $this->belongsToMany(UrlGroup::class, 'url_url_group', 'url_id', 'url_group_id');
+    }
+}
